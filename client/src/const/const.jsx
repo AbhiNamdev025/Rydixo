dc827f0b0fa7c10f3727126eeff3aebaefc6144b
Abhishek Namdev <namdevabhi025@gmail.com>
2025-11-13 20:44:10 +0530

    Update const.jsx

--- client/src/const/const.jsx
+++ client/src/const/const.jsx
@@ -1,5 +1,7 @@
-export const BASE_URL_USER = "http://localhost:2525/user";
-export const BASE_URL_DRIVER = "http://localhost:2525/driver";
-export const BASE_URL_RIDE = "http://localhost:2525/ride";
-export const BASE_URL_AUTH = "http://localhost:2525/auth";
 export const BASE_URL = "http://localhost:2525";
+export const BASE_URL_USER = `${BASE_URL}/user`;
+export const BASE_URL_DRIVER = `${BASE_URL}/driver`;
+export const BASE_URL_RIDE = `${BASE_URL}/ride`;
+export const BASE_URL_AUTH = `${BASE_URL}/auth`;
+
+// export const BASE_URL = "https://rydixo.codrodihub.com";
